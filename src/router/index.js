e4b96bd5fcf3019c1402199e27f0d9333e38a546
wyf <wyi184246@gmail.com>
2026-03-16 20:37:09 +0800

    chore: switch to hash router for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '@/stores'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/login', component: () => import('@/views/login/loginPage.vue') },
     {
